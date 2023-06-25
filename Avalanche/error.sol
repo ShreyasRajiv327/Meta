d9584e89bb4a2b47cdb284527b68a8d9b0c76b98
Shreyas <131350902+ShreyasRajiv327@users.noreply.github.com>
2023-06-25 17:22:24 +0530

    Update error.sol

--- Avalanche/error.sol
+++ Avalanche/error.sol
@@ -1,1 +1,23 @@
 
+//1.SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+contract handle{
+   
+    function product(uint alpha, uint beta)public pure returns(uint){
+        uint prod=alpha*beta;
+        require(prod>1000,"prod should be more than 1000");
+        return prod;
+    }
+    function multiplecheck(uint gamma)public pure returns(uint){
+        if(gamma%3==0){
+            revert("The number is divisible by 3");
+        }
+        return gamma;
+        
+    }
+    function asser(uint alpha)public pure returns(uint){
+        assert(alpha==0);
+        return 1;
+    }
+    
+}
