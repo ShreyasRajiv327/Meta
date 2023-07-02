4f3b464a197e2270834481d07a5a440607ec564f
Shreyas <131350902+ShreyasRajiv327@users.noreply.github.com>
2023-07-02 14:57:52 +0530

    Create deploy.js

---
+++ Avalanche4/deploy.js
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Token = await ethers.getContractFactory("ERC20");
+  const token = await Token.deploy("Degen Gaming Token", "DEGEN"); // Pass the required constructor arguments here
+
+  await token.deployed();
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
