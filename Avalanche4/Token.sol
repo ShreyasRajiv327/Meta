cc3deb72ea7f5ab73fe412452297b3f7fa93f435
Shreyas <131350902+ShreyasRajiv327@users.noreply.github.com>
2023-07-02 22:25:28 +0530

    Update Token.sol

--- Avalanche4/Token.sol
+++ Avalanche4/Token.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    address private owner;
+
+    constructor() ERC20("Degen Gaming Token", "DEGEN") {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function redeem(uint256 amount) external {
+        _burn(msg.sender, amount);
+        // Perform the necessary actions to redeem the tokens for items in the in-game store
+    }
+}
 
